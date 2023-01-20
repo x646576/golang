c51924c18f4c36b0a219028627e62673c190b345
x646576 <x646576@gmail.com>
2023-01-20 10:14:45 +0300

    Update bridge-channel.go
    Concurency in Go. The bridge-channel
    Page 122

--- src/patterns/bridge-channel.go
+++ src/patterns/bridge-channel.go
@@ -1,1 +1,81 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	orDone := func(done, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if ok == false {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+
+	bridge := func(
+		done <-chan interface{},
+		chanStram <-chan <-chan interface{},
+	) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				var stream <-chan interface{}
+				select {
+				case maybeStream, ok := <-chanStram:
+					if ok == false {
+						return
+					}
+					stream = maybeStream
+				case <-done:
+					return
+				}
+				for val := range orDone(done, stream) {
+					select {
+					case valStream <- val:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	genVals := func() <-chan <-chan interface{} {
+		chanStream := make(chan (<-chan interface{}))
+		go func() {
+			defer close(chanStream)
+			for i := 0; i < 10; i++ {
+				stream := make(chan interface{}, 1)
+				stream <- i
+				close(stream)
+				chanStream <- stream
+			}
+		}()
+		return chanStream
+	}
+	for v := range bridge(nil, genVals()) {
+		fmt.Printf("%v ", v)
+	}
+
+}
+
+/*
+0 1 2 3 4 5 6 7 8 9  
+*/
